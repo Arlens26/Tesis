63e9d9005dc823acbfa0ec56e3ba36ba3834c0bc
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-02-23 12:41:01 -0500

    fix(backend): Config cors and change field to description

--- back-platform/courses/models.py
+++ back-platform/courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    decription = models.CharField(blank=True)
+    description = models.CharField(blank=True)
     creditos = models.BigIntegerField()
 
     def __str__(self):

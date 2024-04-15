6d6c4f02ec729d6dea005eeafd48f4a77444c119
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-04-15 16:23:39 -0500

    refactor(frontend): Create Login Form component

--- front-platform/src/App.jsx
+++ front-platform/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css';
 import MonacoEditorWrapper from './components/MonacoEditor';
-import { LogoUnivalleIcon, MenuIcon } from './components/Icons';
+import { MenuIcon } from './components/Icons';
 import { useId, useState } from 'react';
 import { FooterPage } from './pages/Footer.jsx';
 import { HeaderPage } from './pages/Header.jsx';
 import { Link, Routes, Route } from 'react-router-dom';
+import { LoginForm } from './components/LoginForm.jsx';
 import { CourseList } from './components/CourseList.jsx';
 import { CourseForm } from './components/CourseForm.jsx';
 import { EvaluationVersionCourseForm } from './components/EvaluationVersionCourseForm.jsx';
@@ -103,30 +104,6 @@ function App() {
       </>
     )
   }
-
-  function Login() {
-    return (
-      <form className='flex justify-center'>
-        <div className="flex flex-col items-center max-w-sm p-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div className='mb-4'>
-              <LogoUnivalleIcon/>
-            </div>
-            <div className="mb-4">
-              <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre de usuario" required/>
-            </div> 
-            <div className="mb-4">
-              <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Contraseña" required/>
-            </div> 
-            <div className='mb-4'>
-              <button className='bg-primary opacity-80 px-20 py-1 rounded-lg hover:opacity-100 text-slate-100'>Entrar</button>
-            </div>
-            <div className='mb-4'>
-              <span className='text-link cursor-pointer'><a>¿Olvidó su nombre de usuario o contraseña?</a></span>
-            </div>
-        </div>
-      </form>
-    )
-  }
    
   function CreateCode() { 
     const [selectedTheme, setSelectedTheme] = useState(''); // Estado para rastrear el tema seleccionado
@@ -183,7 +160,7 @@ function App() {
         <main className={`bg-main p-6 ${menuChecked ? 'main-menu-open' : ''}`}>
           <Routes>
             <Route path='/' element={<main/>} />
-            <Route path='/login' element={<Login/>} />
+            <Route path='/login' element={<LoginForm/>} />
             <Route path='/code' element={<CreateCode/>}/>
             {/* Cambiar forma de pasar props usando useContext para los cursos */}
             <Route path='/course-list' element={<CourseList courses={courses} getCourse={getCourse} deleteCourse={deleteCourse}/>} />

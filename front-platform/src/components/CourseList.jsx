fd3491fbae293727401686dcd1fffb59cc0c6e36
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-04-15 16:11:24 -0500

    feat(frontend): Create function evaluation version list

--- front-platform/src/components/CourseList.jsx
+++ front-platform/src/components/CourseList.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, version } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCourses } from "../hooks/useCourses";
 import { EditIcon, DeleteIcon } from "./Icons";
 import PropTypes from 'prop-types';
+import { useEvaluationVersionCourse } from "../hooks/useEvaluationVersionCourse";
 
 function BtnCreateCourse() {
     const navigate = useNavigate()
@@ -25,11 +26,66 @@ function BtnCreateCourse() {
     )
 }
 
+function EvaluationVersionList(){
+    const { responseEvaluationVersion } = useEvaluationVersionCourse()
+    console.log(responseEvaluationVersion)
+    const end_date = ''
+
+    return(
+        <>
+        <h1>Evaluation Version Data</h1>
+        <div>
+            {responseEvaluationVersion.map(version => (
+            <div key={version.id}>
+                <p>Version ID: {version.id}</p>
+                <p>Date: {version.date}</p>
+                <p>Course ID: {version.course}</p>
+            </div>
+            ))}
+        </div>
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">Creación evaluación versión curso</th>
+                        <th scope="col" className="px-6 py-3">Fecha de finalización</th>
+                        <th scope="col" className="px-6 py-3">Estado</th>
+                        <th scope="col" className="px-6 py-3">Acciones</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                            {responseEvaluationVersion.map(version => (
+                                <tr key={version.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{version.date}</td>
+                                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"></td>
+                                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{ end_date == '' ? 'Activo' : 'Inactivo' }</td>
+                                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <div className="flex space-x-2">
+                                            <button>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-eye">
+                                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                                    <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+            </table>
+        </>
+    )
+}
+
 export function CourseList() { 
     const { courses, getCourse, deleteCourse } = useCourses()
-    const [accordionStates, setAccordionStates] = useState({});
+    const [accordionStates, setAccordionStates] = useState({})
+    const { responseEvaluationVersion } = useEvaluationVersionCourse()
   
     const navigate = useNavigate()
+
+    const hasEvaluationVersion = responseEvaluationVersion.length > 0
+    console.log(responseEvaluationVersion)
   
     const toggleAccordion = (id) => {
       setAccordionStates((prevState) => ({
@@ -60,6 +116,11 @@ export function CourseList() {
       console.log(selectedCourse)
       navigate('/evaluation-version-course/', {state: { course: selectedCourse}})
       //navigate('/evaluation-version-course/', {state: { courseId }});
+    }
+
+    const handleScheduledCourse = (versionId) =>{
+        console.log(versionId)
+        navigate('/scheduled-course/')
     }
 
     //console.log(courses.length)
@@ -96,14 +157,16 @@ export function CourseList() {
                         }}>
                           <EditIcon />
                         </button>
-                        <button className='bg-primary opacity-80 rounded-sm py-1 px-1 hover:opacity-100'
-                        onClick={(e) => {
-                          // Detener la propagación del evento para que no afecte al botón del acordeón
-                          e.stopPropagation();
-                          handleDeleteCourse(curso.id)
-                        }}>
-                          <DeleteIcon />
-                        </button>
+                        { hasEvaluationVersion ? null : (
+                                <button className='bg-primary opacity-80 rounded-sm py-1 px-1 hover:opacity-100'
+                                    onClick={(e) => {
+                                    // Detener la propagación del evento para que no afecte al botón del acordeón
+                                    e.stopPropagation();
+                                    handleDeleteCourse(curso.id)
+                                 }}> 
+                                <DeleteIcon />
+                                 </button>
+                        )}
                         <span>{curso.name}</span>
                       </div>
                       <svg
@@ -138,6 +201,16 @@ export function CourseList() {
                               <path d="M12 5l0 14" />
                               <path d="M5 12l14 0" />
                           </svg>
+                          <span>Evaluación versión</span>
+                      </button>
+                    <EvaluationVersionList/>
+                    <button 
+                        className='bg-btn-create opacity-80 px-4 py-1 rounded-lg flex items-center hover:opacity-100 text-slate-100'
+                        onClick={(e) =>{
+                          e.stopPropagation();
+                          handleScheduledCourse(version.id)
+                          }}>
+                          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
                           <span>Evaluación versión</span>
                       </button>
                     </div>
--- front-platform/src/hooks/useCourses.js
+++ front-platform/src/hooks/useCourses.js
@@ -22,7 +22,7 @@ export function useCourses() {
     }))
 
     useEffect(() => {
-        getCourses();
+        getCourses()
     }, []);
 
     const getCourses = () => {

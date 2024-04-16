b4381611ebf2fc57e3be8dd4a89ccc33ab69ca64
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-04-15 19:03:40 -0500

    refactor(frontend): Changed navbar style and add name user

--- front-platform/src/pages/Header.jsx
+++ front-platform/src/pages/Header.jsx
@@ -1,9 +1,15 @@
+import { useUsers } from "../hooks/useUsers"
+
 export function HeaderPage() {
+  const { user } = useUsers()
     return (
-        <header className='menu'>     
-          <nav>
-            <div className='logo-container'>
+        <header className=''>     
+          <nav className="grid grid-cols-6 gap-4">
+            <div className='logo-container col-start-1 col-end-3'>
               <img className='logo-img' src="src/images/logo-univalle.png" alt="logo_univalle" />
+            </div>
+            <div className="col-end-7 col-span-2">
+              <span>{user ? <span>Bienvenido, {user.username}</span> : <span>Bienvenido</span>} </span>
             </div>
           </nav>
         </header>

1b74cb6396374e4a96cf5adc2d21d84ae4c504ff
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-04-24 17:19:34 -0500

    fix(frontend): Fix active version id and changed value input with states

--- front-platform/src/components/CourseList.jsx
+++ front-platform/src/components/CourseList.jsx
@@ -1,4 +1,4 @@
-import { useState, version } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { EditIcon, DeleteIcon } from "./Icons";
 //import PropTypes from 'prop-types';
@@ -161,7 +161,18 @@ export function CourseList() {
                             className='bg-btn-create opacity-80 px-4 py-1 rounded-lg flex items-center hover:opacity-100 text-slate-100'
                             onClick={(e) =>{
                               e.stopPropagation();
-                              handleScheduledCourse(curso.id, version.id)
+                               // Filtrar las versiones para encontrar la última versión activa
+                              const activeVersion = evaluationVersion.filter(version => !version.end_date);
+                              
+                              // Verificar si hay alguna versión activa
+                              if (activeVersion.length > 0) {
+                                // Obtener el id de la última versión activa
+                                const activeVersionId = activeVersion[activeVersion.length - 1].id;
+                                console.log(activeVersionId)
+                                handleScheduledCourse(curso.id, activeVersionId)
+                              } else {
+                                console.log("No hay ninguna versión activa para este curso.");
+                              }
                               }}>
                               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
                               <span>Programar curso</span>

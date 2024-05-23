74da3ca712e88558c0ce15a123a3472eb5de75af
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-05-23 17:54:38 -0500

    feat(frontend): Add user profile information in the header

--- front-platform/src/pages/Header.jsx
+++ front-platform/src/pages/Header.jsx
@@ -1,7 +1,8 @@
 import { useUsers } from "../hooks/useUsers"
 
 export function HeaderPage() {
-  const { user } = useUsers()
+  const { user, role } = useUsers()
+  const { profile } = user;
     return (
         <header className=''>     
           <nav className="grid grid-cols-6 gap-4">
@@ -9,7 +10,14 @@ export function HeaderPage() {
               <img className='logo-img' src="src/images/logo-univalle.png" alt="logo_univalle" />
             </div>
             <div className="col-end-7 col-span-2">
-              <span>{user ? <span>Bienvenido, {user.username}</span> : <span>Bienvenido</span>} </span>
+              <span>{user ? <span>Rol: {role} Bienvenido, {user.username}.{' '}
+                {profile && (
+                  <>
+                    {profile.is_professor && <span>Profesor</span>}
+                    {profile.is_director && <span>Director</span>}
+                    {profile.is_student && <span>Estudiante</span>}
+                  </>
+                )}</span> : <span>Bienvenido</span>} </span>
             </div>
           </nav>
         </header>

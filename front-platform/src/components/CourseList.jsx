14e40c5578650a8f250e7829beefaf8dbfa5807f
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-06-02 12:49:35 -0500

    refactor(frontend): Current semester in course list

--- front-platform/src/components/CourseList.jsx
+++ front-platform/src/components/CourseList.jsx
@@ -29,13 +29,16 @@ function BtnCreateCourse() {
 }
 
 const BtnSemesterGroup = ({ onSelectSemester }) => {
+  const currentYear = new Date().getFullYear()
+  const currentSemester = new Date().getMonth() < 6 ? 1 : 2
+
   return (
     <div className="inline-flex rounded-md shadow-sm" role="group">
       <button 
       type="button" 
       className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
       onClick={() => onSelectSemester('current')}>
-        Semestre actual
+        Semestre {currentYear}-{currentSemester}
       </button>
       <button 
       type="button" 

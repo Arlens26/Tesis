9b708d790de019f0570e4adf35bb0b1d8934151b
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-08-10 17:52:46 -0500

    feat(frontend): Create search by filter course and code

--- front-platform/src/components/CourseList.jsx
+++ front-platform/src/components/CourseList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { EditIcon, DeleteIcon } from "./Icons";
 //import PropTypes from 'prop-types';
@@ -56,6 +56,9 @@ export function CourseList() {
     //const { responseEvaluationVersion } = useEvaluationVersionCourse()
     const { evaluationVersion, hasEvaluationVersion } = useEvaluationVersionCourse()
     const [ selectedSemester, setSelectedSemester ] = useState('current')
+    
+    const [search, updateSearch] = useState('')
+    const [filteredCourses, setFilteredCourses] = useState([])
 
     const { role } = useUsers()
   
@@ -64,6 +67,30 @@ export function CourseList() {
     //const hasEvaluationVersion = responseEvaluationVersion.length > 0
     console.log(hasEvaluationVersion)
     console.log(evaluationVersion)
+
+    const getCurrentSemesterCourses = () => {
+      const currentYear = new Date().getFullYear()
+      const currentSemester = new Date().getMonth() < 6 ? 1 : 2
+    
+      return courses.filter(
+        (course) => course.period && course.period.year === currentYear && course.period.semester === currentSemester
+      )
+    }
+    
+    const getPreviousSemesterCourses = () => {
+      const currentYear = new Date().getFullYear()
+      const currentSemester = new Date().getMonth() < 6 ? 1 : 2
+    
+      return courses.filter(
+        (course) => course.period && (course.period.year < currentYear || (course.period.year === currentYear && course.period.semester < currentSemester))
+      )
+    }
+
+    useEffect(() => {
+      const currentSemesterCourses = getCurrentSemesterCourses(courses)
+      const previousSemesterCourses = getPreviousSemesterCourses(courses)
+      filterCourses(search, currentSemesterCourses, previousSemesterCourses)
+    }, [courses, search, role, selectedSemester])
   
     const toggleAccordion = (id) => {
       setAccordionStates((prevState) => ({
@@ -90,7 +117,7 @@ export function CourseList() {
     }
   
     const handleCreateEvaluationVersion = (courseId) =>{
-      const selectedCourse = courses.find(course => course.id === courseId);
+      const selectedCourse = courses.find(course => course.id === courseId)
       console.log(selectedCourse)
       navigate('/evaluation-version-course/', {state: { course: selectedCourse}})
       //navigate('/evaluation-version-course/', {state: { courseId }});
@@ -102,27 +129,49 @@ export function CourseList() {
     }
 
     const handleActivity = (courseId) => {
-      navigate('/activity/', {state: {course_id: courseId}})
+      navigate('/course-list/activity/', {state: {course_id: courseId}})
     }
     
     const handleSelectSemester = (semester) => {
-      setSelectedSemester(semester);
+      setSelectedSemester(semester)
     }
 
-    const currentYear = new Date().getFullYear()
-    const currentSemester = new Date().getMonth() < 6 ? 1 : 2
+    //const currentYear = new Date().getFullYear()
+    //const currentSemester = new Date().getMonth() < 6 ? 1 : 2
     
-    const currentSemesterCourses = courses.filter(
+    /*const currentSemesterCourses = courses.filter(
       (course) => course.period && course.period.year === currentYear && course.period.semester === currentSemester
-    );
+    )
   
     const previousSemesterCourses = courses.filter(
       (course) => course.period && (course.period.year < currentYear || (course.period.year === currentYear && course.period.semester < currentSemester))
-    );
+    )*/
+
+    const filterCourses = (searchTerm, currentSemesterCourses, previousSemesterCourses) => {
+      console.log(searchTerm)
+      const coursesToSearch = role === 'professor'
+        ? (selectedSemester === 'current' ? currentSemesterCourses : previousSemesterCourses)
+        : courses
   
-    const coursesToRender = role === 'professor'
+      if (!searchTerm) {
+        setFilteredCourses(coursesToSearch)
+        return
+      }
+  
+      const filtered = coursesToSearch.filter(course => 
+        course.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        course.code.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+  
+      setFilteredCourses(filtered)
+    }
+
+    const currentSemesterCourses = getCurrentSemesterCourses(courses)
+    const previousSemesterCourses = getPreviousSemesterCourses(courses)
+  
+    const coursesToRender = search ? filteredCourses : (role === 'professor'
     ? (selectedSemester === 'current' ? currentSemesterCourses : previousSemesterCourses)
-    : courses;
+    : courses)
     console.log(coursesToRender)
 
     //console.log(courses.length)
@@ -133,9 +182,41 @@ export function CourseList() {
           </>
       );
   }
+
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const filteredCourses = coursesToRender.filter(
+        (course) =>
+          course.name.toLowerCase().includes(search.toLowerCase()) ||
+          course.code.toLowerCase().includes(search.toLowerCase())
+      )
+      setFilteredCourses(filteredCourses)
+      console.log({ search })
+    }
+
+    const handleChange = (event) => {
+      const newSearch = event.target.value
+      if(newSearch.startsWith(' ')) return
+      updateSearch(event.target.value)
+      const currentSemesterCourses = getCurrentSemesterCourses(courses)
+      const previousSemesterCourses = getPreviousSemesterCourses(courses)
+      filterCourses(newSearch, currentSemesterCourses, previousSemesterCourses)
+    }
   
     return (
       <section>
+        <form className="max-w-md mx-auto m-2" onSubmit={handleSubmit}>   
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input name="search" onChange={handleChange} value={search} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cursos, códigos..." required />
+                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
+            </div>
+        </form>
         <div className='flex flex-col gap-2'>
         {role === 'professor' ? (
           <BtnSemesterGroup onSelectSemester={handleSelectSemester}/>
@@ -143,7 +224,9 @@ export function CourseList() {
         {role === 'director' ? (
           <BtnCreateCourse/>
         ) : null}
-          {coursesToRender.sort((a,b) => a.name > b.name).map((curso) => (
+          {coursesToRender.filter((curso, index, self) => {
+            return self.findIndex((c) => c.id === curso.id) === index
+          }).sort((a,b) => a.name > b.name).map((curso) => (
                 <div key={curso.id} id={`accordion-collapse-${curso.id}`} data-accordion="collapse">
                   <h2 id={`accordion-collapse-heading-${curso.id}`}>
                     <div
@@ -252,7 +335,7 @@ export function CourseList() {
                           handleActivity(curso.id)
                         }}>
                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" /><path d="M12 9v6" /></svg>
-                        <span>Crear actividades</span>
+                        <span>Configurar evaluación</span>
                       </button>  
                     ): null}
                   </div>

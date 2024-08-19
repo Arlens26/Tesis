3dcbeee4d6672792ab7157142b55f4e4b2d90cd8
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-08-19 17:29:19 -0500

    feat(backend): Changed activity serializer

--- back-platform/activities/serializer.py
+++ back-platform/activities/serializer.py
@@ -7,6 +7,18 @@ class ActivitySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ActivityEvaluationDetailSerializer(serializers.ModelSerializer):
+    activity = serializers.SerializerMethodField()
+
+    def get_activity(self, obj):
+        activity_info = {
+            'id': obj.activity.id,
+            'name': obj.activity.name,
+            'description': obj.activity.description,
+            'scheduled_course_id': obj.activity.scheduled_course_id
+        }
+        return activity_info
+
     class Meta:
         model = ActivityEvaluationDetail
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['id', 'percentage', 'activity', 'version_evaluation_detail_id']

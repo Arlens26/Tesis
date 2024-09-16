46578537b9a6362af2d4c4241944f7d22db77dfc
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-09-16 13:39:52 -0500

    refactor(frontend): Filtered by academic period

--- front-platform/src/components/EnrolledStudent.jsx
+++ front-platform/src/components/EnrolledStudent.jsx
@@ -1,8 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as XLSX from 'xlsx'
+import { useEnrolledStudent } from '../hooks/useEnrolledStudent'
 
 export function EnrolledStudent() {
     const [students, setStudents] = useState([])
+    const { getStudentEnrolledCourse, studentEnrolledCourse } = useEnrolledStudent()
+
+    const [selectedPeriodId, setSelectPeriodId] = useState(null)
+    //console.log('Selected period id: ', selectedPeriodId)
+
+    // Verificar que el cursos programados sean únicos
+    const uniqueScheduledCourse = Array.from(
+        new Set(studentEnrolledCourse.map((detail) => detail.scheduled_course.id))
+      ).map((id) => {
+        return studentEnrolledCourse.find((detail) => detail.scheduled_course.id === id)
+      })
+      console.log('Unique Scheduled Course: ', uniqueScheduledCourse)
+
+      const uniqueAcademicPeriod = Array.from(
+        new Set(uniqueScheduledCourse.map((detail) => detail.scheduled_course.academic_period.id))
+      ).map((id) => {
+        return uniqueScheduledCourse.find((detail) => detail.scheduled_course.academic_period.id === id)
+      })
+      console.log('Unique Academic Period: ', uniqueAcademicPeriod)
+
+    const filteredByAcademicPeriod = uniqueScheduledCourse
+        .filter(detail => detail.scheduled_course.academic_period.id == Number(selectedPeriodId))
+    console.log('Filtered by Academic Period: ', filteredByAcademicPeriod)
+
+    useEffect(() => {
+        getStudentEnrolledCourse()
+    }, [])
+
+    useEffect(() => {
+        if (uniqueScheduledCourse && uniqueScheduledCourse.length > 0 && !selectedPeriodId) {
+          setSelectPeriodId(uniqueScheduledCourse[0].scheduled_course.academic_period.id)
+          console.log("Academic Period ID seleccionado:", selectedPeriodId)
+        }
+      }, [uniqueAcademicPeriod, selectedPeriodId])
 
     const handleFileChange = (event) => {
         const file = event.target.files[0]
@@ -25,9 +60,10 @@ export function EnrolledStudent() {
                     return {
                         first_name: item.Nombres,
                         last_name: item.Apellidos,
-                        identification: identification,
+                        identification: item.Cédula,
                         email: item.Correo,
                         code: item.Código,
+                        code_schedule: item.Programa,
                         password: password 
                     };
                 });
@@ -40,6 +76,25 @@ export function EnrolledStudent() {
 
     return(
         <>
+        <label className="text-sm">Periodo académico:</label>
+          <select 
+            id="academic_period" 
+            name='academic_periods' 
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            onChange={(e) => setSelectPeriodId(e.target.value)}
+          >
+            <option disabled>Seleccione un periodo académico</option>
+            {uniqueAcademicPeriod &&
+                uniqueAcademicPeriod.map((detail) => (
+                <option
+                    key={`set_academic_period_${detail.scheduled_course.academic_period.id}`}
+                    value={detail.scheduled_course.academic_period.id}
+                >
+                    {`${detail.scheduled_course.academic_period.year}-${detail.scheduled_course.academic_period.semester}`}
+                </option>
+            ))}
+          </select>
+
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Cargar el archivo
             </label>
@@ -53,7 +108,7 @@ export function EnrolledStudent() {
                     <ul className="list-disc pl-5">
                         {students.map((student, index) => (
                             <li key={index}>
-                                {student.first_name} {student.last_name} - ID: {student.identification} - Email: {student.email} - Código: {student.code} - Contraseña: {student.password}
+                                {student.first_name} {student.last_name} - ID: {student.identification} - Email: {student.email} - Código programa: {student.code_schedule} - Código: {student.code} - Contraseña: {student.password}
                             </li>
                         ))}
                     </ul>

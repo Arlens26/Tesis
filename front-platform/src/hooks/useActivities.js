b67c355e5b8d7eb563138038fc0aedd626e834a9
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-10-31 19:06:31 -0500

    feat(frontend): Get activity evaluation detail

--- front-platform/src/hooks/useActivities.js
+++ front-platform/src/hooks/useActivities.js
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { AuthContext } from "../context/user"
 
 export function useActivities(){
     const ACTIVITY_ENDPOINT = `http://localhost:8000/activities/all/activities/`
     const ACTIVITY_DETAIL_ENDPOINT = `http://localhost:8000/activities/all/activity-evaluation-detail/`
+    const ACTIVITY_EVALUATION_VERSION_DETAIL_ENDPOINT = `http://127.0.0.1:8000/activities/all/activity-version-detail/get_details_by_evaluation_version_detail/?evaluation_version_detail_ids=`
     const [activities, setActivities] = useState({})
     const [activityDetail, setActivityDetail] = useState({})
+
+    const { user } = useContext(AuthContext)
+    const [activityEvaluationDetail, setActivityEvaluationDetail] = useState({})
+    console.log('Activity evaluation detail: ', activityEvaluationDetail)
 
     const getActivities = () => {
         fetch(ACTIVITY_ENDPOINT)
@@ -46,5 +52,33 @@ export function useActivities(){
         });
     }
 
-    return { getActivities, activities, createActivity, getActivityDetail, activityDetail }
+    const getActivityEvaluationVersionDetail = (versionDetailIds) => {
+        const idsString = versionDetailIds.join(',')
+        return fetch(`${ACTIVITY_EVALUATION_VERSION_DETAIL_ENDPOINT}${idsString}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Token ${user.token}`,
+            'Content-Type': 'application/json' 
+          },
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+          }
+          return response.json()
+        })
+        .then(json => {
+          console.log('Preview json activity detail:',json)
+          const { activity_evaluation_detail } = json
+          console.log('New activity evaluation version detail: ', activity_evaluation_detail)
+          setActivityEvaluationDetail(activity_evaluation_detail)
+        })
+        .catch(error => {
+          console.error("Error fetching evaluation version detail:", error)
+          throw error
+        })
+      }
+
+    return { getActivities, activities, createActivity, 
+             getActivityDetail, activityDetail, getActivityEvaluationVersionDetail, activityEvaluationDetail }
 }

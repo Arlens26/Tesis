e84a5e87da1c156a1c1df259049ce1b13dfcadc4
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-11-07 17:26:52 -0500

    feat(backend): Create student grade report serializer

--- back-platform/courses/serializer.py
+++ back-platform/courses/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Course, AcademicPeriod, EvaluationVersion, ScheduledCourse, LearningOutCome, Percentage, EvaluationVersionDetail, StudentEnrolledCourse
+from activities.serializer import ActivityEvaluationDetail, GradeDetailLearningOutCome
 
 class CourseSerializer(serializers.ModelSerializer):
     #period = serializers.StringRelatedField()
@@ -135,6 +136,74 @@ class StudentEnrolledCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentEnrolledCourse
         fields = ['id', 'scheduled_course', 'student', 'active']
+
+class StudentGradeReportSerializer(serializers.Serializer):
+    student_enrolled_course = StudentEnrolledCourseSerializer(source='*')
+    #final_grade = serializers.DecimalField(max_digits=5, decimal_places=2)
+    activity_evaluation_detail = serializers.SerializerMethodField()
+    grade_detail_learning_outcome = serializers.SerializerMethodField()
+
+    def get_activity_evaluation_detail(self, obj):
+        # Obtener actividades y sus detalles de evaluación
+        activity_evaluation_detail = ActivityEvaluationDetail.objects.filter(
+            activity__scheduled_course=obj.scheduled_course
+        )
+        
+        activities_data = []
+        for activity_detail in activity_evaluation_detail:
+            activities_data.append({
+                'id': activity_detail.id,
+                "activity_id": activity_detail.activity.id,
+                "name": activity_detail.activity.name,
+                "description": activity_detail.activity.description,
+                "learning_outcome": activity_detail.version_evaluation_detail.learning_outcome.code,
+                "percentage": activity_detail.percentage
+            })
+        
+        return activities_data if activity_evaluation_detail else None
+    
+    def get_grade_detail_learning_outcome(self, obj):
+        # Filtrar los detalles de calificación por enrolled_course_id
+        enrolled_course_id = obj.id  
+        grade_details = GradeDetailLearningOutCome.objects.filter(
+            enrolled_course_id=enrolled_course_id
+        )
+        
+        # Construir una lista para almacenar los detalles de calificación
+        grade_details_data = []
+        for grade_detail in grade_details:
+            grade_details_data.append({
+                "activity_evaluation_detail_id": grade_detail.activity_evaluation_detail.id,
+                "grade": grade_detail.grade,
+                "code": grade_detail.activity_evaluation_detail.version_evaluation_detail.learning_outcome.code, 
+            })
+        
+        return grade_details_data if grade_details_data else None
+    
+    def to_representation(self, instance):
+        # Llamamos a la representación predeterminada
+        representation = super().to_representation(instance)
+        # Filtramos si están vacíos
+        if not representation.get('activity_evaluation_detail'):
+            representation.pop('activity_evaluation_detail', None)
+        if not representation.get('grade_detail_learning_outcome'):
+            representation.pop('grade_detail_learning_outcome', None)
+        return representation
+
+    #def to_representation(self, instance):
+        ## Estructura el JSON final para consolidar toda la información
+       # representation = super().to_representation(instance)
+        
+        # Información adicional como el cálculo de la nota final puede realizarse aquí
+        #final_grade = GradeDetailLearningOutCome.objects.filter(
+        #    enrolled_course=instance
+        #).aggregate(final_grade=Sum('grade'))['final_grade']
+        
+        #representation['final_grade'] = final_grade
+        #return representation
+
+    class Meta:
+        fields = ['student_enrolled_course', 'activity_evaluation_detail', 'grade_detail_learning_outcome']
 
 
 #class ScheduledCourseDetailSerializer(serializers.ModelSerializer):

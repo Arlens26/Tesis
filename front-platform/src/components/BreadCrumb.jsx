4c942d4b451cde666b14708e896efa8755ae5b1b
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-11-30 17:17:57 -0500

    fix(frontend): Fixed problem with key in bread crumb

--- front-platform/src/components/BreadCrumb.jsx
+++ front-platform/src/components/BreadCrumb.jsx
@@ -20,7 +20,7 @@ export function BreadCrumb(){
                     const to = `/${pathNames.slice(0, index + 1).join('/')}`
                     const isLast = index === pathNames.length - 1;
                     return isLast ? (
-                        <div className="flex items-center">
+                        <div key={index} className="flex items-center">
                             <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
                             </svg>

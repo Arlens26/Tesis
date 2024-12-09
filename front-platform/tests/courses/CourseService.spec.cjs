e78980bc97d6707b51527a003d3d12f3fec2e6d8
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2024-12-09 13:06:26 -0500

    refactor(frontend): Changed test course sevice

--- front-platform/tests/courses/CourseService.spec.cjs
+++ front-platform/tests/courses/CourseService.spec.cjs
@@ -2,10 +2,11 @@ const { test, expect } = require('@playwright/test')
 
 const COURSE_ENDPOINT = `http://127.0.0.1:8000/courses/all/courses/`
 
-test.describe('Course API CRUD', () =>{
-    let courseId
+let courseId
 
-    test.beforeAll(async ({ request }) =>{
+test.describe('Course API CRUD', () =>{
+
+    test.beforeAll('Create course', async ({ request }) =>{
         const response = await request.post(COURSE_ENDPOINT, {
             data: {
                 name: 'Curso de prueba',
@@ -16,6 +17,10 @@ test.describe('Course API CRUD', () =>{
         })
 
         expect(response.ok()).toBeTruthy()
+        if (!response.ok()) {
+            //console.error('Error al crear curso:', response.status(), await response.text())
+            throw new Error('Error al crear curso')
+        }
         const data = await response.json()
         if (!data.id) {
             throw new Error('El curso no fue creado correctamente o no se devolvió el ID')
@@ -24,7 +29,23 @@ test.describe('Course API CRUD', () =>{
         expect(data.name).toBe('Curso de prueba')
     })
 
-    test('Obtener un curso', async ({ request }) => {
+    test('Update course', async ({ request }) => {
+        expect(courseId).toBeDefined()
+        const response = await request.put(`${COURSE_ENDPOINT}${courseId}/`, {
+          data: {
+            name: 'Curso actualizado',
+            code: '751188',
+            description: 'Descripción actualizada',
+            credit: '4',
+          }
+        })
+        
+        expect(response.ok()).toBeTruthy()
+        const updatedData = await response.json()
+        expect(updatedData.name).toBe('Curso actualizado')
+    })
+
+    test('Get course', async ({ request }) => {
         expect(courseId).toBeDefined()
         const response = await request.get(`${COURSE_ENDPOINT}${courseId}/`)
 
@@ -34,31 +55,15 @@ test.describe('Course API CRUD', () =>{
         expect(data.id).toBe(courseId)
     })
 
-    test('Actualizar un curso', async ({ request }) => {
-        expect(courseId).toBeDefined()
-        const response = await request.put(`${COURSE_ENDPOINT}${courseId}/`, {
-          data: {
-            name: 'Curso actualizado',
-            code: '751188',
-            description: 'Descripción actualizada',
-            credit: '4'
-          }
-        })
-        
-        expect(response.ok()).toBeTruthy()
-        const updatedData = await response.json()
-        expect(updatedData.name).toBe('Curso actualizado')
-      })
-
-      test('Eliminar un curso', async ({ request }) => {
+    test('Delete course', async ({ request }) => {
         expect(courseId).toBeDefined()
         const response = await request.delete(`${COURSE_ENDPOINT}${courseId}/`)
-        
+
         expect(response.ok()).toBeTruthy()
         
         // Verificar que el curso ya no existe
         const getResponse = await request.get(`${COURSE_ENDPOINT}${courseId}/`)
         expect(getResponse.status()).toBe(404) // Devuelve 404 si no se encuentra
-      })
+    })
 
 })

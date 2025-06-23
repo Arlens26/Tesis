6addab4e8364710ac18230c21051af3412dd60f4
mauro <mauroatm@gmail.com>
2025-06-23 18:41:43 -0500

    include deployment configuration for render

--- back-platform/class_evaluator/settings.py
+++ back-platform/class_evaluator/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'class_evaluator.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "class_evaluator",
-        "USER": "postgres",
-        "PASSWORD": "palmira_2021@bdP",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+# "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": "class_evaluator",
+#       "USER": "demo",
+#        "PASSWORD": "palmira_2021@bdP",
+ #       "HOST": "127.0.0.1",
+ #       "PORT": "5432",
+#}
 }
 
 

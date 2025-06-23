701bbc5d403bbc15a40119db9b5732520d64fb22
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2025-06-23 17:45:35 -0500

    chore(backend): changed wsgi

--- back-platform/class_evaluator/wsgi.py
+++ back-platform/class_evaluator/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-import sys
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class_evaluator.settings')
@@ -18,5 +16,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'class_evaluator.settings')
 application = get_wsgi_application()
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if path not in sys.path:
-    sys.path.insert(0, path)

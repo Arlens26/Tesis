46a205e4a83edf9be39085a2f17cbf41983769b9
mauro <mauroatm@gmail.com>
2025-06-23 18:48:09 -0500

    include fix

--- back-platform/class_evaluator/settings.py
+++ back-platform/class_evaluator/settings.py
@@ -81,18 +81,14 @@ WSGI_APPLICATION = 'class_evaluator.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-# "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": "class_evaluator",
-#       "USER": "demo",
-#        "PASSWORD": "palmira_2021@bdP",
- #       "HOST": "127.0.0.1",
- #       "PORT": "5432",
-#}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "class_evaluator",
+        "USER": "neondb",
+        "PASSWORD": "npg_gi76CfKIGEBs",
+        "HOST": "ep-long-block-a59bbgdt-pooler.us-east-2.aws.neon.tech",
+        "PORT": "5432",
+        }
 }
 
 
@@ -138,7 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors authorization
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173', 'tesis-0xfh.onrender.com']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [

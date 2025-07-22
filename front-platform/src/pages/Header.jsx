b910db8e6a5bebb87ae717aee6da8bae9d9502ca
Arlens26 <arlenson.herrera@correounivalle.edu.co>
2025-07-22 12:30:50 -0500

    fix(frontend): Fixed header size

--- front-platform/src/pages/Header.jsx
+++ front-platform/src/pages/Header.jsx
@@ -21,12 +21,12 @@ export function HeaderPage() {
   }
   
     return (
-        <header className='bg-white shadow-md p-4 sm:px-6 md:px-8 flex flex-col sm:flex-row justify-between items-center w-full'>     
+        <header className='bg-white shadow-md p-2 sm:px-6 md:px-8 flex flex-col sm:flex-row justify-between items-center w-full'>     
           <nav className="grid grid-cols-6 gap-4">
-            <div className='flex items-center ml-8 mb-8'>
+            <div className='flex items-center ml-16 mb-4'>
               <img className='logo-img h-auto max-w-[160px] max-h-8' src={logo} alt="logo_univalle" />
             </div>
-            <div className="col-end-7 col-span-2 xs:col-end-7 xs:col-span-3 flex justify-end items-center mb-8">
+            <div className="col-end-7 col-span-2 xs:col-end-7 xs:col-span-3 flex justify-end items-center mb-4">
                 {role ? 
                   <>
                     <span>Bienvenido, {user.first_name} {user.last_name} {' '} - {' '}
